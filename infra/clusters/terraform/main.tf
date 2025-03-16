c50c1870e7d8666adbdda24da4adbbb364688ad3
BeardedCrackie <potociarmartin@gmail.com>
2025-03-16 20:07:41 +0100

    chg(clusters): vms resize

--- infra/clusters/terraform/main.tf
+++ infra/clusters/terraform/main.tf
@@ -30,6 +30,8 @@ module "proxmox-ubuntu-vm" {
   source = "./modules/proxmox-ubuntu-vm"
   vm_count  = 3  # This will create 3 VM instances
   vm_name = var.project_name
+  cpu_cores = 4
+  memory_size = 8142
   public_key = local.public_key_content
   vm_username = var.vm_username
 }
