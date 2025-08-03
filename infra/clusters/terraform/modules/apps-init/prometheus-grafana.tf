5a280636e7d3234774b804fc92db3124f3ba9f70
BeardedCrackie <potociarmartin@gmail.com>
2025-08-03 23:14:45 +0200

    feat(terraform): add ArgoCD and Prometheus-Grafana Helm releases with corresponding namespaces

--- infra/clusters/terraform/modules/apps-init/prometheus-grafana.tf
+++ infra/clusters/terraform/modules/apps-init/prometheus-grafana.tf
@@ -1,25 +1,4 @@
 
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
-
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name
-
-  values = [
-    <<-EOF
-    server:
-      service:
-        type: NodePort  # Change to "LoadBalancer" "NodePort" or "ClusterIP" if needed
-    EOF
-  ]
-}
-
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
