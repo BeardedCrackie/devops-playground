14262a1c5ff347e94a0615a8e6248294a45c45cf
BeardedCrackie <potociarmartin@gmail.com>
2025-08-03 23:08:40 +0200

    feat(terraform): refactor microk8s setup and kubeconfig generation into separate playbooks

--- infra/clusters/terraform/modules/ansible-microk8s/main.tf
+++ infra/clusters/terraform/modules/ansible-microk8s/main.tf
@@ -12,7 +12,7 @@ resource "null_resource" "ansible_inventory" {
   }
 }
 
-resource "null_resource" "ansible_setup_microk8s" {
+resource "null_resource" "after_provisionsetup" {
   depends_on = [null_resource.ansible_inventory]
   provisioner "local-exec" {
     command = <<-EOT
@@ -20,15 +20,37 @@ resource "null_resource" "ansible_setup_microk8s" {
       source ../venv/bin/activate
       ../venv/bin/ansible-playbook -i ../ansible/inventory.ini ../ansible/after-provision-setup.yaml \
         -e "ansible_ssh_extra_args='-o StrictHostKeyChecking=no'"
+    EOT
+  }
+}
+
+resource "null_resource" "ansible_setup_microk8s" {
+  depends_on = [null_resource.after_provisionsetup]
+  provisioner "local-exec" {
+    command = <<-EOT
+      # Activate Python virtual environment and run Ansible
+      source ../venv/bin/activate
       ../venv/bin/ansible-playbook -i ../ansible/inventory.ini ../ansible/microk8s-setup.yaml \
         -e "ansible_user=${var.vm_username}"
     EOT
   }
 }
 
-data "local_file" "kubeconfig" {
+resource "null_resource" "ansible_kubeconfig" {
   depends_on = [null_resource.ansible_setup_microk8s]
-  filename   = "../k8s/kubeconfig"
+  provisioner "local-exec" {
+    command = <<-EOT
+      # Activate Python virtual environment and run Ansible
+      source ../venv/bin/activate
+      ../venv/bin/ansible-playbook -i ../ansible/inventory.ini ../ansible/microk8s-kubeconfig.yaml \
+        -e "ansible_user=${var.vm_username}"
+    EOT
+  }
+}
+
+data "local_file" "kubeconfig" {
+  depends_on = [null_resource.ansible_kubeconfig]
+  filename   = "kubeconfig"
 }
 
 output "kubeconfig_path" {
