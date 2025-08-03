9e566acee01cbed664859f045dd75588ed7ac148
BeardedCrackie <potociarmartin@gmail.com>
2025-08-03 20:25:25 +0200

    chore: move provider configurations to a separate file

---
+++ infra/clusters/terraform/providers.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.70.1"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.36.0"
+    } 
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = var.virtual_environment.endpoint
+  username = var.virtual_environment.username
+  password = var.virtual_environment.password
+  insecure  = true
+  ssh {
+    agent    = false
+    private_key = local.priv_key_content
+  }
+}
+
+provider "kubernetes" {
+  config_path = "../k8s/kubeconfig"  # Path to the kubeconfig file from the VM
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "../k8s/kubeconfig"
+  }
+}
