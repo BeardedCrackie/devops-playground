834a0ba676d02fd01733e3354ab26439a34f0f74
BeardedCrackie <potociarmartin@gmail.com>
2025-08-16 21:02:59 +0200

    feat: add monitoring namespace and Helm release for kube-prometheus-stack

---
+++ infra/terraform/k8s-cluster-init/app-monitoring.tf
@@ -0,0 +1,23 @@
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+      "pod-security.kubernetes.io/audit"   = "privileged"
+      "pod-security.kubernetes.io/warn"    = "privileged"
+    }
+  }
+}
+
+resource "helm_release" "kube_prometheus_stack" {
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  create_namespace = false
+
+  depends_on = [
+    kubernetes_namespace.monitoring
+  ]
+}
