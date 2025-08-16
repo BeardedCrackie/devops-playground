31e93c12b4a3d9a2915e1461f4a9384d166789aa
BeardedCrackie <potociarmartin@gmail.com>
2025-08-16 20:03:54 +0200

    refactor: mv MetalLB configurationto separate file

---
+++ infra/terraform/k8s-cluster-init/app-metallb.tf
@@ -0,0 +1,39 @@
+
+resource "kubernetes_namespace" "metallb" {
+  metadata {
+    name = "metallb-system"
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+      "pod-security.kubernetes.io/audit"   = "privileged"
+      "pod-security.kubernetes.io/warn"    = "privileged"
+    }
+  }
+}
+
+resource "helm_release" "metallb" {
+  name       = "metallb"
+  repository = "https://metallb.github.io/metallb"
+  chart      = "metallb"
+  namespace  = kubernetes_namespace.metallb.metadata[0].name
+  create_namespace = false
+  # version omitted to use latest stable
+  depends_on = [
+    kubernetes_namespace.metallb
+  ]
+}
+
+# MetalLB IPAddressPool CR (Layer 2 example)
+resource "kubernetes_manifest" "metallb_ipaddresspool" {
+  manifest = {
+    apiVersion = "metallb.io/v1beta1"
+    kind       = "IPAddressPool"
+    metadata = {
+      name      = "lb-ip"
+      namespace = kubernetes_namespace.metallb.metadata[0].name
+    }
+    spec = {
+      addresses = [var.metallb_ip_pool]
+      autoAssign = true
+    }
+  }
+}
