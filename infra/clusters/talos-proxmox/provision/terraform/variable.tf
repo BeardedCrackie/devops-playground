23eb32a6460adc54fb2b57a9bd4ff229f54f4374
mpotociar <potociarmartin@gmail.com>
2025-08-19 22:17:53 +0200

    refactor: remove unused variables for vm_username and public_key_path in variable.tf

--- infra/clusters/talos-proxmox/provision/terraform/variable.tf
+++ infra/clusters/talos-proxmox/provision/terraform/variable.tf
@@ -13,11 +13,6 @@ variable "project_name" {
     type = string
 } 
 
-variable "vm_username" {
-    type = string
-    default = "ubuntu"
-} 
-
 variable "vm_count" {
     type = number
     default = 1
@@ -31,19 +26,4 @@ variable "priv_key_path" {
 
 locals {
   priv_key_content = file(pathexpand(var.priv_key_path))
-}
-
-variable "public_key_path" {
-    description = "Path to the public key file"
-    type        = string
-    default     = "~/.ssh/id_rsa.pub"
-}
-
-locals {
-  public_key_content = file(pathexpand(var.public_key_path))
-}
-
-variable "image_url" {
-    type = string
-    default = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
 }
