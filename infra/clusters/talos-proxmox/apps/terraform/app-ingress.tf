6f9ab795d11d55ae5247d3f48a8eda2a6c733373
mpotociar <potociarmartin@gmail.com>
2025-08-21 21:20:32 +0200

    refactor: migrate talos infra scripts to infra-playground

--- infra/clusters/talos-proxmox/apps/terraform/app-ingress.tf
+++
@@ -1,7 +0,0 @@
-resource "helm_release" "ingress_nginx" {
-  name       = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  namespace  = "ingress-nginx"
-  create_namespace = true
-}
--- infra/clusters/talos-proxmox/bootstrap/terraform/locals.tf
+++
@@ -1,6 +0,0 @@
-
-locals {
-  controlplane_ip = var.controlplane_ip
-  worker_ips      = var.worker_ips
-  all_nodes       = concat([local.controlplane_ip], local.worker_ips)
-}
